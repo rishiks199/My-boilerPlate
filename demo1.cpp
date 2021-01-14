9dc4aae972f36721ab9cd7451b93fad91c33e2c9
Rishikesh <42427672+rishiks199@users.noreply.github.com>
2021-01-14 22:21:30 +0530

    Add files via upload

---
+++ demo1.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int      long long
+#define pii     pair<int,int>
+#define pb      push_back
+#define mod     10000007
+#define N       1000005
+#define mod1     1e9
+#define mem0(a)    memset(a,0,sizeof(a))
+#define mem1(a)   memset(a,-1,sizeof(a))
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+using namespace std::chrono;
+
+//4-direction
+int x8[8] = { -1, -1, -1, 0, 0, 1, 1, 1 };
+int y8[8] = { -1, 0, 1, -1, 1, -1, 0, 1 };
+
+//4-direction
+int x4[4]={0,0,-1,1};
+int y4[4]={1,-1,0,0};
+
+
+void solve(){
+
+
+}
+
+
+
+signed main(){
+
+    fastio;
+  int test=1;
+    cin>>test;
+    while(test--){
+           solve();
+    }
+
+}
